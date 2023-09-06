e0a16e625b63675525b8cbc96d1d66935ef875fb
Jun Ukemori <jukemori@gmail.com>
2023-09-06 18:31:33 +0900

    added pic

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+      style={[
+        styles.container,
+        {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: "column",
+        },
+      ]}
+    >
+      <Text style={styles.bigBlue}>Hello World</Text>
+      <Text style={styles.red}>Yo</Text>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "skyblue",
+        }}
+      />
+      <View style={{ flex: 1, backgroundColor: "red" }} />
+      <View style={{ flex: 2, backgroundColor: "darkorange" }} />
+      <View style={{ flex: 3, backgroundColor: "green" }} />
+      <Image
+        source={{
+          uri: "https://thumbs.dreamstime.com/b/funny-face-12963753.jpg",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +38,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+  },
+  bigBlue: {
+    color: "blue",
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  red: {
+    color: "red",
   },
 });
